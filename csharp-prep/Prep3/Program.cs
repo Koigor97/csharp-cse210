7d78ab738b84fb8fcca4b793549ede51fd218efd
Koigor97 <turaysamuel101@yahoo.com>
2023-09-12 11:23:05 +0200

    Complete Prep3 learning activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("--------------------------- Welcome to GUESS THE MAGIC 🔮 NUMBER ⁉️ ---------------------------\n");
+        bool isContinue = true;
+
+        static int GetMagicNumber()
+        {
+            Random randomNum = new();
+            return randomNum.Next(1, 101);
+        }
+
+        static void PlayGame()
+        {
+            int magicNumber = GetMagicNumber();
+            int userGuess;
+            Console.WriteLine($"The computer is thinking of a magic number from 1 - 100..🤔");
+            int count = 0;
+            string message = "";
+
+
+            do
+            {
+                
+                Console.Write("Guess the magic number: "); 
+                userGuess = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                if (userGuess == magicNumber)
+                {
+                    message = $"You guessed right 🎉👏🏾!.. The magic 🔮 number is: {magicNumber}\nYou got it right in {count} try(ies).";
+                }
+
+                else if (userGuess > magicNumber)
+                {
+                message = "Guess lower ⬇️";
+                }
+
+                else if (userGuess < magicNumber)
+                {
+                message = "Guess higher ⬆️";
+                }
+
+                count++;
+                Console.WriteLine(message);
+
+            } while (magicNumber != userGuess);
+            
+        }
+
+        while (isContinue)
+        {
+          PlayGame();
+          Console.Write("\nWant to give another try 🥸 ? (type 'Yes' to continue): ");
+          string userChoice = Console.ReadLine().ToLower();
+          isContinue = userChoice == "yes" ? isContinue : false;
+          Console.Clear();
+        }
+    
     }
 }
