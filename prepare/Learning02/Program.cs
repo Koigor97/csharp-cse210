ea94190f4a886b98840d7b51e2f139507aa97e07
Koigor97 <turaysamuel101@yahoo.com>
2023-09-15 21:19:39 +0200

    Complete Learning02 activity.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new(){_jobTitle = "Software Engineer", _company = "IBM", _startYear = 2022, _endYear = 2023};
+
+        Job job2 = new(){_jobTitle = "Senior Frontend Engineer", _company = "BYU", _startYear = 2020, _endYear = 2023};
+
+        Resume myResume = new(){_name = "Samuel Turay"};
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
