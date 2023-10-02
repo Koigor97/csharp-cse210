95e15dee9e2cd51e2f7e1171b72844d529d2067e
Koigor97 <turaysamuel101@yahoo.com>
2023-10-02 17:31:05 +0200

    modified the Learning04 Program class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("Hello Learning04 World!\n");
+        Assignment assignment = new ("Samuel Turay", "Programming");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment mathAssignment = new ("Sarah Alpha", "Fraction", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment writingAssignment = new ("Koirah", "Africa History", "The Period of West Africa Independence");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        
+        
     }
 }
