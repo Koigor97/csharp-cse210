9993ec337dd2f7afa90f04f2359298b8212b646f
Koigor97 <turaysamuel101@yahoo.com>
2023-10-09 22:28:10 +0200

    Finish base class - Shape.cs

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,27 @@
+using System;
+// The base/parent class
+
+public class Shape
+{
+    private string _color;
+
+    public Shape (string color)
+    {
+        _color = color;
+    }
+
+    public void SetColor (string theColor)
+    {
+        _color = theColor;
+    }
+
+    public string GetColor ()
+    {
+        return _color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 3.0;
+    }
+}
