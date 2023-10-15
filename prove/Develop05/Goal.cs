23209de0d3e60333c92ad46a8598f7ed6336a2b8
Koigor97 <turaysamuel101@yahoo.com>
2023-10-15 23:41:22 +0200

    modify Goal class

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -4,13 +4,15 @@ public  abstract class Goal
 {
     protected string _shortName;
     protected string _description;
-    protected int _points;
+    protected int _setPoints;
+    protected int _currentPoint = 0;
+    protected string _checkBox = "[ ]";
 
     public Goal (string name, string description, int points)
     {
         _shortName = name;
         _description = description;
-        _points = points;
+        _setPoints = points;
     }
 
     public abstract void RecordEvent();
@@ -19,8 +21,39 @@ public  abstract class Goal
 
     public virtual string GetDetailsString()
     {
-        return $"[ ] {_shortName} - {_description}";
+        return $"{_checkBox} {_shortName} - {_description}";
     }
 
     public abstract string GetStringRepresentation();
+
+    public int GetCurrentPoint()
+    {
+        return _currentPoint;
+    }
+
+    public string GetGoalName()
+    {
+        return _shortName;
+    }
+
+    public int GetSetPoint()
+    {
+        return _setPoints;
+    }
+
+    public void AddSetPointToCurrentPoint()
+    {
+        _currentPoint += _setPoints;
+    }
+
+    public void SetCheckMark()
+    {
+        string newCheckBox = _checkBox.Replace(_checkBox, "[✓]");
+        _checkBox = newCheckBox;
+    }
+
+    public string GetCheckMark()
+    {
+        return _checkBox;
+    }
 }
