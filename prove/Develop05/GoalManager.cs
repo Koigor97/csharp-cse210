b156186eba9c422382b150c3d0bbac4f3a1adc80
Koigor97 <turaysamuel101@yahoo.com>
2023-10-15 23:39:45 +0200

    modify GoalManager class

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -3,7 +3,9 @@
 public class GoalManager
 {
     private List<Goal> _goals = new ();
-    private int _score;
+    private int _score = 0;
+
+    private int _count = 0;
 
     private string _folderPath = "savefolder/";
 
@@ -14,7 +16,7 @@ public class GoalManager
 
     public void Start()
     {
-        string theMenu = "1. Create New Goal ⚽️ | 2. List Goals 📝 | 3. Save Goals 🗳️ | 4. Load Goals ♽ | 5. Record Event ✍🏼 | 6. Quit ❌\n";
+        string theMenu = "\n1. Create New Goal ⚽️ | 2. List Goals 🧾 | 3. Save Goals 🗳️ | 4. Load Goals ♽ | 5. Record Event 📝 | 6. Quit ❌\n";
         bool isAppOn = true;
 
         do
@@ -29,7 +31,8 @@ public class GoalManager
            }
            else if (userChoice == 2)
            {
-                ListGoalDetails();
+                Console.WriteLine("");
+                ListGoals();
            }
            else if (userChoice == 3)
            {
@@ -54,24 +57,48 @@ public class GoalManager
 
     private  void DisplayPlayerPoint()
     {
-
+        Console.WriteLine($"You now have {_score} points");
     }
 
     private void ListGoalNames()
     {
-
+        if (_goals.Count != 0)
+        {
+            foreach (Goal goal in _goals)
+            {
+                _count++;
+                Console.WriteLine($"{_count}. {goal.GetGoalName()}");
+            }
+            _count = 0;
+        }
+        else 
+        {
+            Console.WriteLine("🗑️ - The list of goals is empty. Either load your saved goals or create new ones");
+        }
     }
 
     private void ListGoalDetails()
     {
-
+        if (_goals.Count != 0)
+        {
+            foreach (Goal goal in _goals)
+            {
+                _count++;
+                Console.WriteLine($"{_count}. {goal.GetDetailsString()}");
+            }
+            _count = 0;
+        }
+        else 
+        {
+            Console.WriteLine("🗑️ - The list of goals is empty. Either load your saved goals or create new ones");
+        }
     }
 
     private void CreateGoal()
     {
-        string[] goalTypes = {"Simple Goal", "Eternal Goals", "Checklist Goals"};
-        Console.WriteLine($"The types of goals are: 1. {goalTypes[0]} | 2. {goalTypes[1]} | 3. {goalTypes[2]}\n");
-        Console.Write("What type of goal do you want create?: ");
+        string[] goalTypes = {"😌 Simple Goal", "😇 Eternal Goals", "☑️  Checklist Goals"};
+        Console.WriteLine($"\nThe types of goals are: 1. {goalTypes[0]} | 2. {goalTypes[1]} | 3. {goalTypes[2]}");
+        Console.Write("\nWhat type of goal do you want create?: ");
         int typeOfGoal = int.Parse(Console.ReadLine()) - 1;
 
         if (typeOfGoal == 0)
@@ -91,23 +118,61 @@ public class GoalManager
         }
         else
         {
-            Console.WriteLine("Invalid: This option does not exist.");
+            Console.WriteLine("❗️Invalid: This option does not exist.");
         }
     }
 
     private void RecordEvent()
     {
+        ListGoalNames();
+        Console.Write("\nWhich goal did you accomplished: ");
+        int goalCompleteIndex = int.Parse(Console.ReadLine());
 
+        Goal goalAccomplished = _goals[goalCompleteIndex - 1];
+        goalAccomplished.isComplete(true);
+        _score += goalAccomplished.GetCurrentPoint();
+
+        string congratMessage = $"\n🎉Congratulations🎉! You have earned {goalAccomplished.GetSetPoint()}";
+        Console.WriteLine(congratMessage);
+        DisplayPlayerPoint();
+    }
+
+    private void ListGoals()
+    {
+        ListGoalDetails();
     }
 
     private void SaveGoal()
     {
+        Console.Write("\nWhat would you like to name the file? : ");
+        string fileName = Console.ReadLine();
 
+        using StreamWriter saveGoals = new($"{_folderPath}{fileName}.txt");
+        saveGoals.WriteLine(_score);
+        foreach (Goal goal in _goals)
+        {
+            saveGoals.WriteLine(goal.GetStringRepresentation());
+        }
+        _goals.Clear();
+        
     }
 
     private void LoadGoals()
     {
+        string[] files = Directory.GetFiles(_folderPath); 
+        Console.WriteLine("These are the saved files 📑:");
+        foreach (string file in files)    
+        {
+            _count++;
+            Console.WriteLine($"{_count}. {Path.GetFileNameWithoutExtension(file)} 📄");
+        }
+        _count = 0;
 
+        Console.Write("Choose the file to load (by it number): ");
+        int choosenFile = int.Parse(Console.ReadLine());
+        string[] fileContent = File.ReadAllLines(files[choosenFile - 1]);
+
+        _goals.AddRange(ConvertToGoalObjects(fileContent));
     }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -121,30 +186,65 @@ public class GoalManager
 
     private int SetGoalPoint()
     {
-        Console.Write("Enter the amount of point you want to achieve: ");
+        Console.Write("\nEnter the amount of point you want to achieve: ");
         int _goalPoint = int.Parse(Console.ReadLine());
         return _goalPoint;
     }
 
     private string SetGoalDescription()
     {
-        Console.Write("Write a short description of the goal: ");
+        Console.Write("\nWrite a short description of the goal: ");
         string _goalDescription = Console.ReadLine();
         return _goalDescription;
     }
 
     private int SetBonusPoint()
     {
-        Console.Write("Enter the amount of bonus point you want to achieve for this goal: ");
+        Console.Write("\nEnter the amount of bonus point you want to achieve for this goal: ");
         int _bonusPoint = int.Parse(Console.ReadLine());
         return _bonusPoint;
     }
 
     private int SetCheckListCount()
     {
-        Console.Write("How many times do you want to set for this goal to be completed? : ");
+        Console.Write("\nHow many times do you want to set for this goal to be completed? : ");
         int _checklistCount = int.Parse(Console.ReadLine());
         return _checklistCount;
     }
     
+    private List<Goal> ConvertToGoalObjects(string[] param)
+    {
+        char colon = ':';
+        char pipe = '|';
+        int start = 1;
+        int end = param.Length;
+        param = param.Where((value, index) => index >= start && index <= end).ToArray();
+
+        List<Goal> initialGoal = new();
+
+        foreach (string list in param)
+        {
+            string[] parts = list.Split(colon);
+            string goalName = parts[0];
+            string[] contents = parts[1].Split(pipe);
+
+            if (goalName == "😌 Simple Goal")
+            {
+                SimpleGoal simpleGoal = new(name: contents[0], description: contents[1], points: int.Parse(contents[2]), goal: goalName);
+                initialGoal.Add(simpleGoal);
+            }
+            else if (goalName == "😇 Eternal Goals")
+            {
+                EternalGoal eternalGoal = new (name: contents[0], description: contents[1], points: int.Parse(contents[2]), goal: goalName);
+                initialGoal.Add(eternalGoal);
+            }
+            else if (goalName == "☑️ Checklist Goals")
+            {
+                CheckListGoal checkListGoal = new(name: contents[0], description: contents[1], points: int.Parse(contents[2]), goal: goalName, bonus: int.Parse(contents[3]), target: int.Parse(contents[4]));
+                initialGoal.Add(checkListGoal);
+            }
+        }
+        Console.WriteLine("\nFile loaded Successfully ✅");
+        return initialGoal;
+    }
 }
