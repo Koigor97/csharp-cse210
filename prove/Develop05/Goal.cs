165436bbdbfa093ae1de5e98e0447b4247f247e6
Koigor97 <turaysamuel101@yahoo.com>
2023-10-15 17:28:09 +0200

    complete Goal.cs template

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,26 @@
+
+
+public  abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal (string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool isComplete(bool param);
+
+    public virtual string GetDetailsString()
+    {
+        return $"[ ] {_shortName} - {_description}";
+    }
+
+    public abstract string GetStringRepresentation();
+}
