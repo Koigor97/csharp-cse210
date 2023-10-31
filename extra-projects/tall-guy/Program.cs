8db6e02b95ca7d08dc195bcc288023ee672db291
Koigor97 <turaysamuel101@yahoo.com>
2023-10-31 16:22:33 +0200

    Update TallGuy class and Program.cs to include a new method for dunking and improve overall code structure.

--- extra-projects/tall-guy/Program.cs
+++ extra-projects/tall-guy/Program.cs
@@ -1,10 +1,42 @@
 ﻿
 using System;
 
+public class TallGuy : IDunk {
+    public string _Name;
+    public double _Height;
+
+    public TallGuy (string name, double hieght)
+    {
+        _Name = name;
+        _Height = hieght;
+    }
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine($"My name is {_Name} and I'm {_Height} inches tall.");
+    }
+
+    public string DunkWordsToSay
+    {
+        get { return "Double beepbop 🥴";}
+    }
+
+    public string SwirlyDunk()
+    {
+        return "ElSquirlTorDunkdo...!!!";
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        // Console.WriteLine("Hello World");
+        TallGuy tallGuy = new (name: "Samuel", hieght: 6.7);
+        tallGuy.TalkAboutYourself();
+
+        Console.WriteLine($"{tallGuy._Name} is gonna go {tallGuy.DunkWordsToSay} on you. {tallGuy.SwirlyDunk()} mode.. activated ✅");
+
     }
 }
+
+
