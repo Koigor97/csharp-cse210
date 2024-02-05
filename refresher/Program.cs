137dd958b8c505e3f52f30f2326fc8b532b5199d
Koigor97 <turaysamuel101@yahoo.com>
2024-02-05 14:15:50 +0200

    add the refresher folder

---
+++ refresher/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace refresher;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string prompt = "What is your name: ";
+        Console.Write(prompt);
+        string response = Console.ReadLine();
+
+        string output = $"Hello {response}. Nice to meet you";
+        Console.WriteLine(output);
+
+    }
+}
