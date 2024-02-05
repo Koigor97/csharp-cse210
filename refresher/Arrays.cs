cd04c2534205e812404b11444f6abf9bf680e483
Koigor97 <turaysamuel101@yahoo.com>
2024-02-05 21:27:20 +0200

    create and add Array class

---
+++ refresher/Arrays.cs
@@ -0,0 +1,13 @@
+
+public class Array
+{
+    public Array GetArray(int num)
+    {
+        int [] array = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            array[1] = 1**2;
+        }
+        return array;
+    }
+}
