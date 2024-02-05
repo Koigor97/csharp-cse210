58214496b51f5fdd1b6a0d0d848ecc4908fa5f1c
Koigor97 <turaysamuel101@yahoo.com>
2024-02-05 21:27:46 +0200

    instantiate Array object

--- refresher/Program.cs
+++ refresher/Program.cs
@@ -4,12 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        Array array = new Array(); 
+
         string prompt = "What is your name: ";
         Console.Write(prompt);
         string response = Console.ReadLine();
 
         string output = $"Hello {response}. Nice to meet you";
         Console.WriteLine(output);
+
+        DateTime dateOfBirth = new DateTime(1997, 12, 3);
+        Console.WriteLine(dateOfBirth.DayOfWeek);
+
+        int[] Array arrayList = array.GetArray(6);
+        foreach (int element in arrayList)
+        {
+            Console.WriteLine(element);
+        }
 
     }
 }
